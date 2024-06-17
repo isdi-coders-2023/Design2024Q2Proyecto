02df10a74f1385a04c9730cab961ee9ac806e227
Pako <pako.lora@interficie.com>
2024-06-18 00:48:44 +0200

    feat: document id refactor scenario

---
+++ 3rdparty/rate-me/src/libs/AWSStorage.ts
@@ -0,0 +1,13 @@
+export class AWSStorage {
+  constructor() {
+    // super loud and slow constructor
+  }
+
+  public store(fileContent: string): string {
+    //generate path
+    const filePath = 'some-random-path';
+    //store file
+    //return destination path
+    return filePath;
+  }
+}
